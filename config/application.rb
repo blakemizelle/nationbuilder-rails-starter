46489836d85110b8699160c292f251d650724063
Blake Mizelle <blake.mizelle@nationbuilder.com>
2025-10-03 11:08:47 -0400

    Fix Rails autoloading for service classes
    - Added app/services to autoload_paths and eager_load_paths
    - This fixes NameError for OAuth and NationBuilder modules
    - Services are now properly loaded by Rails

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,10 @@ module NbRailsApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # Autoload app/services directory
+    config.autoload_paths << Rails.root.join("app/services")
+    config.eager_load_paths << Rails.root.join("app/services")
 
     # Don't generate system test files.
     config.generators.system_tests = nil
